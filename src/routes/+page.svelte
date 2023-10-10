3544831b6a9e842e04b6989f1a4aec1b66d2c0f2
ScriptKiddingBaby <scriptkidding1@gmail.com>
2023-10-10 09:59:58 +0800

    first commity

---
+++ src/routes/+page.svelte
@@ -0,0 +1,26 @@
+<script>
+	// @ts-ignore
+	import Carousel from 'svelte-carousel';
+	import { browser } from '$app/environment';
+  
+	/**
+	 * @type {{ goToNext: () => void; }}
+	 */
+	let carousel; // for calling methods of the carousel instance
+	
+	const handleNextClick = () => {
+	  carousel.goToNext()
+	}
+  </script>
+  
+  {#if browser}
+	<Carousel
+	  bind:this={carousel}
+	>
+	  <div>1</div>
+	  <div>2</div>
+	  <div>3</div>
+	</Carousel>
+  {/if}
+  
+  <button on:click={handleNextClick}>Next</button>
